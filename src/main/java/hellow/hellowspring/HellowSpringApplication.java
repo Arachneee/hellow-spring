8dc6f823af4d6b19e15c3ef828cca16dd125eb7c
Arachne <jhg2819@naver.com>
2023-07-01 00:18:55 +0900

    start

--- src/main/java/hellow/hellowspring/HellowSpringApplication.java
+++ src/main/java/hellow/hellowspring/HellowSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HellowSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HellowSpringApplication.class, args);
 	}
