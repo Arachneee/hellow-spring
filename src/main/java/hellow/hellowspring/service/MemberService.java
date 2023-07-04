e5c19255d624b88a18a5707a377e4ce01907481a
Arachne <jhg2819@naver.com>
2023-07-04 22:39:20 +0900

    jpa

--- src/main/java/hellow/hellowspring/service/MemberService.java
+++ src/main/java/hellow/hellowspring/service/MemberService.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import hellow.hellowspring.domain.Member;
 import hellow.hellowspring.repository.MemberRepository;
 import hellow.hellowspring.repository.MemoryMemberRepository;
 
-
+@Transactional
 public class MemberService {
 	private final MemberRepository memberRepository;
 
